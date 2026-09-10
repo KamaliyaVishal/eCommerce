9bbead526dc16b6b4e8ab9a70db6114855a2b83c
Vishal <vishal.kamaliya.er@gmail.com>
2026-09-10 20:44:48 +0530

    Added support for MS Discovery service and API Gateway

--- inventory-service/src/main/java/com/inventory_service/controller/ProductController.java
+++ inventory-service/src/main/java/com/inventory_service/controller/ProductController.java
@@ -4,21 +4,35 @@ import com.inventory_service.dto.ProductResponseDto;
 import com.inventory_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/products")
+@RequestMapping
 public class ProductController {
 
     private final ProductService productService;
+    private final DiscoveryClient discoveryClient;
+    private final RestClient restClient;
+
+    @GetMapping("/test")
+    public String getOrder() {
+        ServiceInstance instanceInfo = discoveryClient.getInstances("order-service").getFirst();
+        return restClient.get()
+                .uri(instanceInfo.getUri()+"/order")
+                .retrieve()
+                .body(String.class);
+    }
 
     @GetMapping
     public ResponseEntity<List<ProductResponseDto>> getAllInventory() {
